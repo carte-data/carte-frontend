534fdb5a7360dc19597b17d0dc1e8557afbb7eab
Balint Haller <balint.haller@shapr3d.com>
2020-12-17 22:13:13 +0100

    main: fix getStaticProps

--- pages/dataset/[connection]/[database]/[dataset].jsx
+++ pages/dataset/[connection]/[database]/[dataset].jsx
@@ -1,42 +1,62 @@
+import fs from 'fs';
+import path from 'path';
+import frontmatter from 'front-matter';
+
 import { useRouter } from 'next/router';
 import { getPaths } from '../../../../lib/get-paths';
 
-const DatasetPage = () => {
-    const router = useRouter()
-    const {
-        connection,
-        database,
-        dataset
-    } = router.query
+const DATASET_SOURCE_DIR = '/data/datasets';
 
-    return (
-        <div className="table">
-            <div>Connection: <code>{connection}</code></div>
-            <div>Database: <code>{database}</code></div>
-            <div>Dataset: <code>{dataset}</code></div>
-        </div>
-    )
+const DatasetPage = (props) => {
+  const router = useRouter();
+  const { connection, database, dataset } = router.query;
+  const { metadata, content } = props;
+
+  return (
+    <div className="table">
+      <div>
+        Connection: <code>{connection}</code>
+        Table conn: <code>{metadata.connection}</code>
+      </div>
+      <div>
+        Database: <code>{database}</code>
+      </div>
+      <div>
+        Dataset: <code>{dataset}</code>
+      </div>
+      <div>
+        Type: <code>{metadata.table_type}</code>
+      </div>
+    </div>
+  );
 };
 
-export default DatasetPage
+export default DatasetPage;
 
 export async function getStaticPaths() {
-  const datasets = getPaths('./data/datasets')
-  const paths = datasets.map((datasetPath) => `/dataset/${datasetPath}`)
+  const datasets = await getPaths(DATASET_SOURCE_DIR);
+  const paths = datasets.map((datasetPath) =>
+    path.join('/dataset', datasetPath)
+  );
 
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps(context) {
-  const {
+  const { connection, database, dataset } = context.params;
+  const datasetsDirectory = path.join(
+    process.cwd(),
+    DATASET_SOURCE_DIR,
     connection,
     database,
-    dataset
-  } = context.params;
+    dataset + '.md'
+  );
 
-
+  const filePath = path.join(datasetsDirectory);
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const { attributes: metadata, body: content } = frontmatter(fileContents);
 
   return {
-    props: {}
-  }
+    props: { metadata, content },
+  };
 }
