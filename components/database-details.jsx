38e46ec55cea75905f5eadd45de727af40b44265
Balint Haller <balint.haller@shapr3d.com>
2021-01-03 15:56:14 +0100

    main: use items for structure subitems key

--- components/database-details.jsx
+++ components/database-details.jsx
@@ -1,15 +1,16 @@
 import Link from 'next/link';
 
 const DatasetRow = ({ name, url }) => {
-  return <span className="block text-lg"><Link href={url}>{name}</Link></span>;
+  return <span className="block hover:text-blue-400 mb-1"><Link href={url}>{name}</Link></span>;
 };
 
 const DatabaseDetails = ({ connection, database, structure }) => {
-  const datasets = structure[connection].databases[database].datasets;
+  const datasets = structure[connection].items[database].items;
   return (
     <div>
-      <span className="bold">Database: </span>
-      <code>{database}</code>
+      <h1 className="text-2xl font-semibold text-gray-800">{database}</h1>
+      <h2 className="mb-6 text-gray-400">Database</h2>
+      <h2 className="text-gray-700 mt-8 mb-2 text-xl font-medium">Tables:</h2>
       <div>
         {datasets.map((dataset) => (<DatasetRow name={dataset.name} url={dataset.url}/>))}
       </div>
