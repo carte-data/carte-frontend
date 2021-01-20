4cb70f2f98501236cab5ef3c6085380499602c4f
Balint Haller <balint.haller@shapr3d.com>
2021-01-20 19:10:04 +0100

    main: rename components to uppercase

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     <div className="w-full mx-auto">
       <Header />
       <main>
-        <div className="my-12 sm:px-6 mx-auto px-12 sm:px-6 lg:px-32 flex flex-row">
-          <nav className="lg:w-80 w-full">
+        <div className="my-12 sm:px-6 mx-auto px-12 sm:px-6 lg:px-32 flex flex-row w-full flex-wrap">
+          <nav className="lg:w-80 w-1/3 md:w-50 mb-8 border-b md:border-0 border-gray-200 pb-4 w-full">
             <Sidebar />
           </nav>
           <div className="flex-grow">{children}</div>
